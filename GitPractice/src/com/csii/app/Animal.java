8b37711d31956789a342f3c85f173bc00b24ebc6
王佰伟 <1753749110@qq.com>
2020-10-20 22:41:09 +0800

    Revert "Revert "animal.java""
    This reverts commit ea78458dbccba2eaf7ec9bceba887410a7ea2e76.

--- GitPractice/src/com/csii/app/Animal.java
+++ GitPractice/src/com/csii/app/Animal.java
@@ -2,6 +2,12 @@ package com.csii.app;
 
 public class Animal {
 	
+	public static void main(String[] args) {
+		System.out.println("反转提交");
+		
+		System.out.println("my name is wang");
+	}
+	
 	
 	
 
