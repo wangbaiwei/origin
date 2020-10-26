af7d2fb69889fd5e88fbcc8f73936611dc4c22a8
小明 <1753749110@qq.com>
2020-10-26 14:30:26 +0800

    2

--- GitPractice/src/com/csii/test/PatternTest.java
+++ GitPractice/src/com/csii/test/PatternTest.java
@@ -4,7 +4,7 @@ public class PatternTest {
 	
 	
 	public static void main(String[] args) {
-		String str = "My name is wang ";
+		String str = "My name is wang Hello this is ....";
 		System.out.println(str.matches("^My*"));
 	}
 
