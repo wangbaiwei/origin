23a8b1d5e482505f0af5d45977dfe0b148f30d33
王佰伟 <1753749110@qq.com>
2023-02-04 17:32:54 +0800

    tank-rename

---
+++ tank-v1/src/test/java/com/wbw/BufferedImageTest.java
@@ -0,0 +1,20 @@
+package com.wbw;
+
+import org.junit.jupiter.api.Test;
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+public class BufferedImageTest {
+
+//    @Test
+//    public void test() throws IOException {
+//        BufferedImage read = ImageIO.read(new File("C:/Users/Youth/Downloads/3a43f4eed2b74939807ea05328d69f3c/马士兵老师_坦克大战_java基础(一期)/tank/src/images/BadTank1.png"));
+//        Assert.assertNotNull(read);
+//
+//        BufferedImage read1 = ImageIO.read(this.getClass().getClassLoader().getResourceAsStream("images/BadTank1.png"));
+//        Assert.assertNotNull(read1);
+//    }
+}
+
