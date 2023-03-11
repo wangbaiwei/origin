3819dac0f7291eea50269b7dc73dcbc01618a7e6
王佰伟 <1753749110@qq.com>
2023-03-11 12:03:31 +0800

    校验验证码

--- online-taxi-public/api-passenger/src/main/java/com/wbw/apipassenger/service/VerificationCodeService.java
+++ online-taxi-public/api-passenger/src/main/java/com/wbw/apipassenger/service/VerificationCodeService.java
@@ -3,10 +3,13 @@ package com.wbw.apipassenger.service;
 import com.wbw.apipassenger.remote.ServiceVerificationcodeClient;
 import com.wbw.apipassenger.response.TokenResponse;
 import com.wbw.internalcommon.dto.ResponseResult;
+import com.wbw.internalcommon.internalcommon.constant.CommonStatusEnum;
 import com.wbw.internalcommon.response.NumberCodeResponse;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
+
 import java.util.concurrent.TimeUnit;
 
 @Service
@@ -29,7 +32,7 @@ public class VerificationCodeService {
         int numberCode = numberCodeResponse.getData().getNumberCode();
         System.out.println("remote number code: " + numberCode);
 
-        String key = verificationCodePrefix + passengerPhone;
+        String key = generatorKeyByPhone(passengerPhone);
         stringRedisTemplate.opsForValue().set(key, numberCode + "", 2, TimeUnit.MINUTES);
 
         // 通过短信服务商，将对应的验证码发送到手机上，以阿里短信服务，腾讯短信通，华信，客联
@@ -37,19 +40,35 @@ public class VerificationCodeService {
 
     }
 
+    private String generatorKeyByPhone(String passengerPhone) {
+        return verificationCodePrefix + passengerPhone;
+    }
+
 
     /**
      * 校验验证码
-     * @param passengerPhone 手机号
+     *
+     * @param passengerPhone   手机号
      * @param verificatoinCode 验证码
      * @return
      */
     public ResponseResult checkCode(String passengerPhone, String verificatoinCode) {
         // 根据手机号到redis读取验证码
         System.out.println("根据手机号到redis读取验证码");
-
+        // 生成key
+        String key = generatorKeyByPhone(passengerPhone);
+        // 更具key获取value
+        String codeReids = stringRedisTemplate.opsForValue().get(key);
+        System.out.println(key); // passenger-verification-code-18669945566
+        System.out.println("redis中的value: " + codeReids);
         // 校验验证码
-        System.out.println("校验验证码");
+        if (StringUtils.isBlank(codeReids)) {
+            return ResponseResult.fail(CommonStatusEnum.VERIFICATION_CODE_ERROR.getCode(), CommonStatusEnum.VERIFICATION_CODE_ERROR.getValue());
+        }
+
+        if (!verificatoinCode.trim().equals(codeReids.trim())) {
+            return ResponseResult.fail(CommonStatusEnum.VERIFICATION_CODE_ERROR.getCode(), CommonStatusEnum.VERIFICATION_CODE_ERROR.getValue());
+        }
 
         // 判断原来是否有用户，并进行对应的处理
         System.out.println("判断原来是否有用户，并进行对应的处理");
--- online-taxi-public/internal-common/src/main/java/com/wbw/internalcommon/internalcommon/constant/CommonStatusEnum.java
+++ online-taxi-public/internal-common/src/main/java/com/wbw/internalcommon/internalcommon/constant/CommonStatusEnum.java
@@ -5,6 +5,10 @@ import lombok.Getter;
 
 public enum CommonStatusEnum {
 
+    /**
+     * 错误验证码：1000-1099
+     */
+    VERIFICATION_CODE_ERROR(1099, "验证码不正确"),
     SUCCESS(1, "success"),
     FAIL(0, "fail");
 
