ca2d5e3f4f5cdc155ba34181d06bda799e92eb8a
王佰伟 <1753749110@qq.com>
2023-03-16 22:17:53 +0800

    api-boss

---
+++ online-taxi-public/servce-dirver-user/src/test/java/com/wbw/servcedirveruser/ServceDirverUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wbw.servcedirveruser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServceDirverUserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
