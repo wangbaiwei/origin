7d04a87ddd1f4197000830995f9c9f595c275dde
王佰伟 <1753749110@qq.com>
2023-03-23 23:57:30 +0800

    service-driver-user扩展字段desc

--- service-dirver-user/src/main/java/com/wbw/servcedirveruser/service/CarService.java
+++ service-dirver-user/src/main/java/com/wbw/servcedirveruser/service/CarService.java
@@ -1,12 +1,12 @@
 package com.wbw.servcedirveruser.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.wbw.internalcommon.dto.Car;
 import com.wbw.internalcommon.dto.ResponseResult;
 import com.wbw.internalcommon.response.TerminalResponse;
 import com.wbw.internalcommon.response.TrackResponse;
 import com.wbw.servcedirveruser.mapper.CarMapper;
-import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.wbw.servcedirveruser.remote.ServiceMapClient;
 import lombok.extern.slf4j.Slf4j;
 import net.sf.json.util.JSONUtils;
@@ -38,12 +38,15 @@ public class CarService extends ServiceImpl<CarMapper, Car> implements IService<
 
     public ResponseResult addCar(Car car) {
 
-        // 获得车辆的tid
-        ResponseResult<TerminalResponse> responseResult = serviceMapClient.addTerminal(car.getVehicleNo());
-        log.info("创建终端接口响应信息：{}", JSONUtils.valueToString(responseResult));
         LocalDateTime now = LocalDateTime.now();
         car.setGmtModified(now);
         car.setGmtCreate(now);
+        carMapper.insert(car);
+
+        // 获得车辆的tid
+        ResponseResult<TerminalResponse> responseResult = serviceMapClient.addTerminal(car.getVehicleNo(), car.getId() + "");
+        log.info("创建终端接口响应信息：{}", JSONUtils.valueToString(responseResult));
+
         String tid = responseResult.getData().getTid();
         car.setTid(responseResult.getData().getTid());
 
@@ -57,8 +60,8 @@ public class CarService extends ServiceImpl<CarMapper, Car> implements IService<
         car.setTrname(trname);
         log.info("获得车辆的轨迹trid：{}, trname:{}", trid, trname);
 
-        int insert = carMapper.insert(car);
-        return ResponseResult.success(insert);
+        carMapper.updateById(car);
+        return ResponseResult.success();
     }
 
     public ResponseResult<Car>  getCarById(Long carId) {
